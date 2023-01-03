924e4f19cb3288be84349d200a40e3ac21f94920
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-01-03 16:44:54 +0600

    Great

--- Rahat/CSE/admin.py
+++ Rahat/CSE/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 #import Student model from models.py
 from .models import Student
+from .models import User
 #migrate the model to admin panel
 admin.site.register(Student)
+admin.site.register(User)
--- Rahat/CSE/models.py
+++ Rahat/CSE/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django import forms
+
 
 # Create your models here.
 #create a model for the database
@@ -20,3 +22,22 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+class User(models.Model):
+    id= models.AutoField(primary_key=True)
+    name= models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email= models.EmailField()
+    blood_group= models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    joining_date=models.DateField()
+    sales_count=models.IntegerField()
+    city= models.CharField(max_length=255)
+    division= models.CharField(max_length=255)
+    address= models.TextField()
+    post_code= models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name+'( '+ self.phone +')'
+
